effd8f06e69074806288714e09abb6cfdcd4a8e2
takuto-info <164002258+takuto-info@users.noreply.github.com>
2024-03-20 08:31:38 +0900

    Add files via upload

---
+++ randomobject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomobject : MonoBehaviour {
+    [SerializeField]
+    GameObject prefab;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("Generate", 1, 1);
+    }
+    public float span = 10f;
+    private float currentTime = 0f;
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+        if (currentTime > span)
+        {
+            float x = Random.Range(-5f, 5f);
+            float y = 5;
+            float z = Random.Range(0f, 250f);
+            Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity);
+            currentTime = 0f;
+        }
+    }
+}
---
+++ start.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class start : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("game easy");
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("game nomal");
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("game hard");
+        }
+    }
+}
