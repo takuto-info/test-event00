a076cace5e88e7d466afebabf871f1aab8c5deae
takuto-info <164002258+takuto-info@users.noreply.github.com>
2024-03-19 19:14:04 +0900

    Add files via upload

---
+++ bullt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullt : MonoBehaviour
+{
+    public GameObject Cube;
+
+
+    // Use this for initialization
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        Instantiate(Cube,this.transform.position, Quaternion.identity);
+    }
+}
